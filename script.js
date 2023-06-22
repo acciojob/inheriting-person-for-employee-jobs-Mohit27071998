14ffd924a8c01482271a42fff5a8122bb5f1f647
Mohit Kamdi <130160983+Mohit27071998@users.noreply.github.com>
2023-06-22 11:19:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-// complete this js code
+//your code here
 function Person(name, age) {
-	this.name = name;
-	this.age = age;
-  }
-  Person.prototype.greet(){
-	  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
-	
+  this.name = name;
+  this.age = age;
 }
 
+Person.prototype.greet = function () {
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+};
+
 function Employee(name, age, jobTitle) {
-	Person.call(this,name,age)
-	this.jobTitle = jobTitle;
-	}
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
 
-Employee.prototype = Object.create(Person.prototype)
-	Employee.prototype.constructor = Employee
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
 
-	Employee.prototype.jobGreet() {
- console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)		
-	}
-// Do not change code below this line
+Employee.prototype.jobGreet = function () {
+  console.log(
+    `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+  );
+};
+
 window.Person = Person;
 window.Employee = Employee;
